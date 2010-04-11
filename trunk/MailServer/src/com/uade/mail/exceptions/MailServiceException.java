3c98b9dcb2ba3a880cde3d9bdd2f61f59a5a959a
ejonatan.castro <ejonatan.castro@564b3e00-e694-7b7e-ef56-043036fbc020>
2010-04-11 23:01:16 +0000

    Initial Commit

---
+++ trunk/MailServer/src/com/uade/mail/exceptions/MailServiceException.java
@@ -0,0 +1,12 @@
+package com.uade.mail.exceptions;
+
+import java.rmi.RemoteException;
+
+public class MailServiceException extends RemoteException{
+
+	/**
+	 * Clase por default utilizada para indicar excepciones en el servidor de mail
+	 */
+	private static final long serialVersionUID = -4921089313468081959L;
+
+}
